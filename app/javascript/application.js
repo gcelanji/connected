50341e7c2ef06a501a2927449782fae55603a557
Gerald <geraldcelanji@gmail.com>
2022-11-22 18:01:28 +0000

    Added posts controller and view for creating a post

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
 
 import "jquery"
 import "jquery_ujs"
 import "popper"
 import "bootstrap"
+// Deactivated Turbo so Device errors can display properly
+Turbo.session.drive = false
