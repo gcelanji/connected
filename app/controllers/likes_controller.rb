58360f4ca9ecbdfbcecf4442830389baa4d30d0c
Gerald Celanji <geraldcelanji@gmail.com>
2022-12-23 17:12:15 +0000

    add and delete likes on posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,20 +6,22 @@ class LikesController < ApplicationController
       flash[:notice] = @like.errors.full_messages.to_sentence
     end
 
-    redirect_to @like.post
+    # redirect_to @like.post
+    redirect_back_or_to @like.post
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
     post = @like.post
     @like.destroy
-    redirect_to post
+    # redirect_to post
+    redirect_back_or_to post
   end
 
   private
 
   def like_params
-    binding.irb
+    # binding.irb
     params.require(:like).permit(:post_id)
   end
 end
