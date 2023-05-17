2fa098ebd1b943ba40c8b05a74badf6fd61fa517
Gerald Celanji <geraldcelanji@gmail.com>
2023-05-17 19:43:19 +0100

    Connections index page wip

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,4 +1,11 @@
 class ConnectionsController < ApplicationController
+  def index
+    @pending_requests = Connection.where(connection_id: current_user.id, status: 'pending')
+    accepted_requests_sent_by_user = Connection.where(user_id: current_user.id, status: 'accepted')
+    accepted_requests_received_by_user = Connection.where(connection_id: current_user.id, status: 'accepted')
+    @connections = accepted_requests_sent_by_user + accepted_requests_received_by_user
+  end
+
   def create
     @user_to_connect = User.find(params[:user_id])
     @pending_connection = current_user.connections.build(connection: @user_to_connect, status: :pending)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   resources :likes, only: [:create, :destroy]
-  resources :connections, only: [:create, :destroy]
+  resources :connections, only: [:index, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
